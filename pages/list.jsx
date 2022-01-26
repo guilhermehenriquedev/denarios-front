2045db2c029305c68ddfa2d20a4b6b8dfa4ca7bc
Guilherme Henrique <guilhermebass27@gmail.com>
2022-01-26 02:04:38 -0300

    corrige erro

--- pages/list.jsx
+++ pages/list.jsx
@@ -2,12 +2,13 @@ import { API_URL } from '@/config/index'
 import Grid from '@material-ui/core/Grid'
 import NewsItem from '@/components/NewsItem'
 export default function Lists({ news }) {
-
+console.log('data....: ', news.data)
   return (
     <div>
       <Grid pb={4} container spacing={7} alignItems="flex-center" justifyContent="center">
         {news.length === 0 && <h3>No News</h3>}
-        {news.map((item) => (
+
+        {/*news.map((item) => (
           <Grid
             item
             xs={10}
@@ -16,14 +17,15 @@ export default function Lists({ news }) {
           >
             <NewsItem news={item} />
           </Grid>
-        ))}
+        ))*/}
+
       </Grid>
     </div>
   )
 }
 
 export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/posts`)
+  const res = await fetch(`${API_URL}/exchanges/list/`)
   const news = await res.json()
 
   return {
